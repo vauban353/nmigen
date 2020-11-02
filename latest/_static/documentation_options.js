cd3e65d6bfb32aa89eac68f4fe7521d94a3248fb
whitequark <whitequark@whitequark.org>
2020-11-02 06:20:42 +0000

    Deploying to gh-pages from  @ 781a3aa76776532657d79ea195a1ac28f18b23d6 🚀

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.dev202',
+    VERSION: '0.3.dev203',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
